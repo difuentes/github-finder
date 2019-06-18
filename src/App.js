1082864c2496d053f2bcc18839ccb5f61b83a44f
Alejandro Hernández <alejandrehl@icloud.com>
2019-06-18 00:04:04 -0400

    Users components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
 import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <UserItem />
+      <Users />
     </div>
   );
 }

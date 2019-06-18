7919f302dcdac9eabe6ab594406bb0d7987ff650
Alejandro Hernández <alejandrehl@icloud.com>
2019-06-18 00:10:20 -0400

    List and passing state with props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
 import Users from './components/users/Users'
 import './App.css';
 
@@ -8,7 +7,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Users />
+      <div className="container">
+        <Users />
+      </div>
     </div>
   );
 }

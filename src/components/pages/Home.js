2cb910c3f9301355f3e07979b8ce042dc128a464
difuentes <fuentesdiego054@gmail.com>
2020-09-26 16:24:40 -0300

    Quitar Fragmet
    eliminar fragment

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Search from '../users/Search'
 import Users from '../users/Users'
 
 const Home = () => 
-    <Fragment>
+    <>
         <Search />
         <Users />
-    </Fragment>
+    </>
 
 export default Home
